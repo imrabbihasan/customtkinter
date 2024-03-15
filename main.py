119b39b3a31879553dcf1cd19a64f31049a24373
imrabbihasan <rabbihossain270@gmail.com>
2024-03-15 18:53:38 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import customtkinter
+
+customtkinter.set_appearance_mode("System ")
+customtkinter.set_default_color_theme("green")
+
+root = customtkinter.CTk()
+root.geometry("500x380")
+
+
+def login():
+    print("Login Successful")
+
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=30, padx=60, fill="both", expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="Login System", font=("Roboto", 24), )
+label.pack(pady=12, padx=10)
+
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
+entry1.pack(pady=12, padx=10)
+
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
+entry2.pack(pady=12, padx=10)
+
+button = customtkinter.CTkButton(master=frame, text="Login", command=login)
+button.pack(pady=12, padx=10)
+
+signupbutton = customtkinter.CTkButton(master=frame, text="Signup", command=login, fg_color="red",)
+signupbutton.pack(pady=12, padx=10)
+
+checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember Me", )
+checkbox.pack(pady=12, padx=10)
+
+
+root.mainloop()
